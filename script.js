ee9b1981078c81171af27133d7e9067b9ff086d3
nitish <nitishmishra0006@gmail.com>
2024-08-04 01:55:33 +0530

    Quote Generator

---
+++ script.js
@@ -0,0 +1,19 @@
+const quotesArrayData = []
+
+async function fetchData(){
+    const response = await fetch('https://dummyjson.com/quotes?limit=1000')
+    const data = await response.json()
+
+    quotesArrayData.push(...data.quotes)
+}
+
+function getRandomQuotes(quotesArray){
+    return Math.floor(Math.random()*quotesArray)
+}
+
+async function getQuotes(){
+    await fetchData()
+    let randomNumber = getRandomQuotes(quotesArrayData.length)
+    document.getElementById('quote').innerHTML = ' " ' + quotesArrayData[randomNumber].quote + ' " '
+    document.getElementById('author').innerHTML = ' - ' + quotesArrayData[randomNumber].author
+}
